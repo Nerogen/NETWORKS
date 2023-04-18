9bc6e03cec34dbb8039c272a818a682aa447d2a4
Nero <14k21096@gmail.com>
2023-04-18 17:01:09 +0300

    update

--- Lab1/client.py
+++ Lab1/client.py
@@ -1,13 +1,19 @@
 import socket
+import sys
 
 from Lab1.config import encoding, address
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(address)
 
 post = """POST /auth HTTP/1.1\r\nContent-Type: text/html; charset=utf-8\r\n\r\nuserName=Ganesh&password=pass"""
 get = """GET /home HTTP/1.1\r\nContent-Type: text/html; charset=utf-8\r\n\r\n"""
 
-content = f"{get}".encode(encoding)
-client.send(content)
-print(client.recv(1024).decode(encoding))
+
+def main(argument):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(address)
+    content = f"{get}".encode(encoding)
+    client.send(content)
+    print(client.recv(1024).decode(encoding))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
